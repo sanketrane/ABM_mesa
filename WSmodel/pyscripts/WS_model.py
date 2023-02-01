54018b3a7c31394e8bc65b7ed1d08c61c5aa24e5
Sanket <sanketrn@gmail.com>
2023-02-01 00:23:52 -0500

    space modeule exploration

--- WSmodel/pyscripts/WS_model.py
+++ WSmodel/pyscripts/WS_model.py
@@ -8,25 +8,41 @@ class MoneyAgent(mesa.Agent):
         super().__init__(unique_id, model)
         self.wealth = 1
 
+    def move(self):
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos, moore=True, include_center=False
+        )
+        new_position = self.random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
+
+    def give_money(self):
+        cellmates = self.model.grid.get_cell_list_contents([self.pos])
+        if len(cellmates) > 1:
+            other_agent = self.random.choice(cellmates)
+            other_agent.wealth += 1
+            self.wealth -= 1
+
     def step(self):
-        if self.wealth == 0:
-            return
-        other_agent = self.random.choice(self.model.schedule.agents)
-        other_agent.wealth += 1
-        self.wealth -= 1
+        self.move()
+        if self.wealth > 0:
+            self.give_money()
 
 
 class MoneyModel(mesa.Model):
     """A model with some number of agents."""
 
-    def __init__(self, N):
+    def __init__(self, N, width, height):
         self.num_agents = N
+        self.grid = mesa.space.MultiGrid(width, height, True)
         self.schedule = mesa.time.RandomActivation(self)
         # Create agents
         for i in range(self.num_agents):
             a = MoneyAgent(i, self)
             self.schedule.add(a)
+            # Add the agent to a random grid cell
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(a, (x, y))
 
     def step(self):
-        """Advance the model by one step."""
         self.schedule.step()
--- WSmodel/pyscripts/runMod.py
+++ WSmodel/pyscripts/runMod.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from WS_model import *
 
 
+'''
 all_wealth = []
 # This runs the model 100 times, each model executing 10 steps.
 for j in range(100):
@@ -16,3 +18,44 @@ for j in range(100):
 
 plt.hist(all_wealth, bins=range(max(all_wealth) + 1))
 plt.show()
+'''
+
+model = MoneyModel(50, 10, 10)
+for i in range(20):
+    model.step()
+
+agent_wealth = [a.wealth for a in model.schedule.agents]
+p1 = plt.figure(1)
+plt.hist(agent_wealth)
+p1.show()
+
+agent_counts = np.zeros((model.grid.width, model.grid.height))
+for block in model.grid.coord_iter():
+    block_content, x, y = block
+    agent_count = len(block_content)
+    agent_counts[x][y] = agent_count
+
+
+agent_loc = [a.pos for a in model.schedule.agents]
+
+# open file in write mode
+with open('sales.txt', 'w') as fp:
+    for a in agent_loc:
+        line = ' '.join(str(x) for x in a)
+        fp.write("%s\n" % line)
+    print('Done')
+    
+p2 = plt.figure(3)
+plt.imshow(agent_counts, interpolation="nearest")
+plt.colorbar()
+
+p2.show()
+
+'''
+block_wealth = np.zeros((model.grid.width, model.grid.height))
+for block in model.grid.coord_iter():
+    block_content, wealth = block
+
+
+'''
+input()
